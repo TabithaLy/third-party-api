3185f3b581c514bf7161e3898d1e3b8a7fd7291a
Tabitha <spencersalmon.tabitha@gmail.com>
2022-07-28 21:40:09 -0400

    javascript variables and html initial structure for planner

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// declare variables from given style.css
+var past = $('.past');
+var present = $('.present');
+var future = $('.future');
+var currentDay = $('.currentDay');
+
+// hour variables
+
+var nine = $('.nine').text('9AM');
+var ten = $('.ten').text('10AM');
+var eleven = $('.eleven').text('11AM');
+var noon = $('.noon').text('12PM');
+var one = $('.one').text('1PM');
+var two = $('.two').text('2PM');
+var three = $('.three').text('3PM');
+var four = $('.four').text('4PM');
+var five = $('.five').text('5PM');
